618cfe05d09f21c42398d41e3c7ba5d1612ac0b5
honey96dev <honey96dev@gmail.com>
2019-08-02 19:40:31 -0800

    Update start-mysqlapp.js

--- bin/start-mysqlapp.js
+++ bin/start-mysqlapp.js
@@ -69,14 +69,20 @@ if (cluster.isWorker) {
     setTimeout(fftService.getLastTimestamp, 15000, 'tETHUSD', '1h', (symbol, binSize, timestamp) => {
         fftService.calculateFFT(symbol, binSize, timestamp);
     });
+    setTimeout(fftService.getLastTimestamp, 5000, 'tBCHUSD', '5m', (symbol, binSize, timestamp) => {
+        fftService.calculateFFT(symbol, binSize, timestamp);
+    });
+    setTimeout(fftService.getLastTimestamp, 15000, 'tBCHUSD', '1h', (symbol, binSize, timestamp) => {
+        fftService.calculateFFT(symbol, binSize, timestamp);
+    });
 
-    //fft-hist
-    setTimeout(fftService1.getLastTimestamp, 5000, 'tBCHUSD', '5m', (symbol, binSize, timestamp) => {
-        fftService1.calculateFFT(symbol, binSize, timestamp);
-    });
-    setTimeout(fftService1.getLastTimestamp, 15000, 'tBCHUSD', '1h', (symbol, binSize, timestamp) => {
-        fftService1.calculateFFT(symbol, binSize, timestamp);
-    });
+    // //fft-hist
+    // setTimeout(fftService1.getLastTimestamp, 5000, 'tBCHUSD', '5m', (symbol, binSize, timestamp) => {
+    //     fftService1.calculateFFT(symbol, binSize, timestamp);
+    // });
+    // setTimeout(fftService1.getLastTimestamp, 15000, 'tBCHUSD', '1h', (symbol, binSize, timestamp) => {
+    //     fftService1.calculateFFT(symbol, binSize, timestamp);
+    // });
 
     //bitmex instruments
     bitmexInstrumentService.downloadInstrument();
