9272cda510211d20b81599723955d04a0b240d78
honey96dev <honey96dev@gmail.com>
2019-08-02 23:13:59 -0800

    Update start-mysqlapp.js

--- bin/start-mysqlapp.js
+++ bin/start-mysqlapp.js
@@ -19,8 +19,8 @@ if (cluster.isMaster) {
 
 if (cluster.isWorker) {
     //trades bucket
-    setTimeout(bitmexTradeBucketService1.getLastTimestamp, 0, 'XBTUSD', '1m', (symbol, binSize, timestamp) => {
-        bitmexTradeBucketService1.downloadTradeBucketed(symbol, binSize, timestamp);
+    setTimeout(bitmexTradeBucketService.getLastTimestamp, 0, 'XBTUSD', '1m', (symbol, binSize, timestamp) => {
+        bitmexTradeBucketService.downloadTradeBucketed(symbol, binSize, timestamp);
     });
     setTimeout(bitmexTradeBucketService.getLastTimestamp, 3000, 'XBTUSD', '5m', (symbol, binSize, timestamp) => {
         bitmexTradeBucketService.downloadTradeBucketed(symbol, binSize, timestamp);
