81111ce0975b827d03c4100aa12a0b22e183b701
honey96dev <honey96dev@gmail.com>
2019-08-03 01:30:22 -0800

    Update start-mysqlapp.js

--- bin/start-mysqlapp.js
+++ bin/start-mysqlapp.js
@@ -74,12 +74,12 @@ if (cluster.isWorker) {
     setTimeout(fftService.getLastTimestamp, 15000, 'XBTUSD', '1h', (symbol, binSize, timestamp) => {
         fftService.calculateFFT(symbol, binSize, timestamp);
     });
-    // setTimeout(fftService.getLastTimestamp, 5000, 'tETHUSD', '5m', (symbol, binSize, timestamp) => {
-    //     fftService.calculateFFT(symbol, binSize, timestamp);
-    // });
-    // setTimeout(fftService.getLastTimestamp, 15000, 'tETHUSD', '1h', (symbol, binSize, timestamp) => {
-    //     fftService.calculateFFT(symbol, binSize, timestamp);
-    // });
+    setTimeout(fftService.getLastTimestamp, 5000, 'tETHUSD', '5m', (symbol, binSize, timestamp) => {
+        fftService.calculateFFT(symbol, binSize, timestamp);
+    });
+    setTimeout(fftService.getLastTimestamp, 15000, 'tETHUSD', '1h', (symbol, binSize, timestamp) => {
+        fftService.calculateFFT(symbol, binSize, timestamp);
+    });
     // setTimeout(fftService.getLastTimestamp, 5000, 'tBCHUSD', '5m', (symbol, binSize, timestamp) => {
     //     fftService.calculateFFT(symbol, binSize, timestamp);
     // });
@@ -87,13 +87,13 @@ if (cluster.isWorker) {
     //     fftService.calculateFFT(symbol, binSize, timestamp);
     // });
 
-    // fft-hist
-    setTimeout(fftService1.getLastTimestamp, 5000, 'tETHUSD', '5m', (symbol, binSize, timestamp) => {
-        fftService1.calculateFFT(symbol, binSize, timestamp);
-    });
-    setTimeout(fftService1.getLastTimestamp, 15000, 'tETHUSD', '1h', (symbol, binSize, timestamp) => {
-        fftService1.calculateFFT(symbol, binSize, timestamp);
-    });
+    // // fft-hist
+    // setTimeout(fftService1.getLastTimestamp, 5000, 'tETHUSD', '5m', (symbol, binSize, timestamp) => {
+    //     fftService1.calculateFFT(symbol, binSize, timestamp);
+    // });
+    // setTimeout(fftService1.getLastTimestamp, 15000, 'tETHUSD', '1h', (symbol, binSize, timestamp) => {
+    //     fftService1.calculateFFT(symbol, binSize, timestamp);
+    // });
 
     //bitmex instruments
     bitmexInstrumentService.downloadInstrument();
